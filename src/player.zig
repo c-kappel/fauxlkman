b468f3d457070036a5eecab4b62a006f775772cd
c-kappel <chrisjkappel@gmail.com>
2026-08-24 20:48:01 -0400

    types

--- src/player.zig
+++ src/player.zig
@@ -1,1 +1,11 @@
-const Song = struct { artist: u32, duration: u32 };
+const Song = struct {
+    artist: u32,
+    duration: u32,
+    timestamp: u32,
+};
+
+const Playlist = struct {
+    songs: [250]Song,
+};
+
+pub fn createPlaylist(name: []const u8) void {}
